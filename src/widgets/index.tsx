857916378e390ce548ac01f2290d8d491620299c
zitongcharliedeng <108423881+zitongcharliedeng@users.noreply.github.com>
2025-04-10 00:04:14 +0000

    rollback: remove floatingwidget feature in favour of a task queue implanting approach for the taskcard, and creation of new task status tags

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -8,54 +8,7 @@ import {
 import '../style.css';
 import '../App.css';
 
-async function showTaskcardInbox(
-  plugin: RNPlugin,
-  classContainer?: string,
-  position?: { top?: number; bottom?: number; left?: number; right?: number },
-) {
-  await plugin.window.openFloatingWidget(
-    "taskcard_inbox_popup",
-    position ?? { top: 0, left: 0 },
-    classContainer
-  );
-}
-
-async function getCurrentFlashcardQueueDimensions(): Promise<{ width: number, height: number }> {
-  let elementWidth = 500;
-  let elementHeight = 500;
-
-  const element = document.querySelector(`.rn-queue`);
-  if (element) {
-    const rect = element.getBoundingClientRect();
-    elementWidth = rect.width;
-    elementHeight = rect.height;
-  }
-
-  return {
-    width: elementWidth,
-    height: elementHeight,
-  }
-}
-
-async function updateTaskcardPopupDimensions(plugin: ReactRNPlugin): Promise<void> {
-  await plugin.app.registerWidget(
-    "taskcard_inbox_popup",
-    WidgetLocation.FloatingWidget,
-    {
-      dimensions: await getCurrentFlashcardQueueDimensions()
-    }
-  );
-}
-
 async function onActivate(plugin: ReactRNPlugin) {
-  plugin.event.addListener(AppEvents.QueueEnter, undefined, async () => {
-    setTimeout(() => {
-        updateTaskcardPopupDimensions(plugin),
-        showTaskcardInbox(plugin, "rn-queue");
-    }, 25);
-  });
 }
-
 async function onDeactivate(_: ReactRNPlugin) { }
-
 declareIndexPlugin(onActivate, onDeactivate);
