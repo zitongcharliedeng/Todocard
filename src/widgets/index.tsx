dc36e9a54036403267eedf8a97031dd67c9dd57b
zitongcharliedeng <108423881+zitongcharliedeng@users.noreply.github.com>
2025-04-10 00:38:34 +0000

    styles: make todo with taskcarddoing tag have red background

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -9,6 +9,14 @@ import '../style.css';
 import '../App.css';
 
 async function onActivate(plugin: ReactRNPlugin) {
+  plugin.app.registerCSS(
+    "#content",
+    `
+      [data-rem-container-tags*="todo"][data-rem-container-tags*="taskcarddoing"] > * {
+        background-color: rgba(255, 0, 0, 0.25) !important;
+      }
+    `
+  );
 }
 async function onDeactivate(_: ReactRNPlugin) { }
 declareIndexPlugin(onActivate, onDeactivate);
